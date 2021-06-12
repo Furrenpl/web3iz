632d183441dca8a1aaa63b877b79c2a8b621256f
Furrenpl <84564976+Furrenpl@users.noreply.github.com>
2021-06-13 02:49:34 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -96,21 +96,8 @@ def brightness(filename,cho):
  #plt.show()
  plt.savefig(gr_path1)
  plt.close()
-
-##делаем график разности
- fig2 = plt.figure(figsize=(6, 4))
- ax = fig.add_subplot()
- data2 = np.random.randint(0, 255, (100, 100))
- ax.imshow(img, cmap='plasma')
- b2 = ax.pcolormesh(data2, edgecolors='black', cmap='plasma')
- fig2.colorbar(b2, ax=ax)
- gr_path2 = "./static/newgr2.png"
- sns.displot(data2)
- #plt.show()
- plt.savefig(gr_path2)
- plt.close()
  
- return output_filename,gr_path,gr_path1,gr_path2
+ return output_filename,gr_path,gr_path1
 
 
 
@@ -124,7 +111,7 @@ def net():
  newfilename=None
  grname=None
  grname1=None
- grname2=None
+
  # проверяем нажатие сабмит и валидацию введенных данных
  if form.validate_on_submit():
   # файлы с изображениями читаются из каталога static
@@ -132,11 +119,11 @@ def net():
   ch=form.cho.data
  
   form.upload.data.save(filename)
-  newfilename,grname,grname1,grname2 = brightness(filename,ch)
+  newfilename,grname,grname1 = brightness(filename,ch)
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети если был нажат сабмит, либо передадим falsy значения
  
- return render_template('net.html',form=form,image_name=newfilename,gr_name=grname,gr_name1=grname1,gr_name2=grname2)
+ return render_template('net.html',form=form,image_name=newfilename,gr_name=grname,gr_name1=grname1)
 
 
 if __name__ == "__main__":

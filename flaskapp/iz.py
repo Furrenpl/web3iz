11c4d8282f0f11d8124ccaf50d96f1420a59ec92
Furrenpl <84564976+Furrenpl@users.noreply.github.com>
2021-06-13 21:38:54 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -108,12 +108,12 @@ def brightness(filename,cho):
  px1 = imgend.load()
  for i in range(img.size[0]): # for every pixel:
   for j in range(img.size[1]):
-   pixel = px[x, y]
+   pixel = px[i, j]
    r, g, b = pixel[0], pixel[1], pixel[2]
    r1sum = r1sum + r
    g1sum = g1sum + g
    b1sum = b1sum + b
-   pixel1 = px1[x, y]
+   pixel1 = px1[i, j]
    r, g, b = pixel1[0], pixel1[1], pixel1[2]
    r2sum = r2sum + r
    g2sum = g2sum + g
